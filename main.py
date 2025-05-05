5d5a1aa57f31de9e402ca3ee64e730a1ec947981
YJ <jyjin1206@gmail.com>
2025-05-05 12:29:40 +0900

    WebScoket 구현 중

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, WebSocket
 from fastapi.middleware.cors import CORSMiddleware
 from utils.youtube_utils import extract_youtube_stream_url
 from utils.process_video_utils import process_video
 from utils.youtube_subtitle_utils import get_subtitle_text
 from models.request_models import YouTubeRequest, SubtitleRequest
 from models.response_models import ProcessResponse, SubtitleResponse, TestResponse
+from utils.similarity_compare_utils import fetch_landmark, compare_landmark
 
 from fastapi import Depends
 from sqlalchemy.orm import Session
-from db.session import get_db
+from db.session import get_db, SessionLocal
 from crud.game import get_game_by_url
 
 import uvicorn
@@ -48,6 +49,56 @@ async def process_youtube(req: YouTubeRequest, db: Session = Depends(get_db)):
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+@app.websocket("/ws/landmark")
+async def landmark_socket(websocket: WebSocket):
+    await websocket.accept()
+    scores = []
+    video_url = None
+    landmark = None
+    db : Session = SessionLocal()
+    try:
+        while True:
+            data = await websocket.receive_json()
+
+            if not video_url:
+                video_url = data["video_url"]
+                game = get_game_by_url(db, youtube_link=video_url)
+                landmark = game.landmark
+            
+            start_ms = data["start_ms"]
+            end_ms = data["end_ms"]
+            user_landmark = data["user_landmark"]
+            is_last_sentence = data.get("is_last_sentence", False)
+
+            reference = fetch_landmark(start_ms, end_ms)
+            user_processed = [x["landmark"] for x in user_landmark]
+            score = compare_landmark(reference, user_processed)
+
+            scores.append(score)
+
+            # 실시간 개별 점수 전송
+            await websocket.send_json({
+                "sentence_score": score,
+                "index": len(scores) - 1
+            })
+
+            # 마지막 문장이면 전체 점수도 함께 전송 후 종료료
+            if is_last_sentence:
+                await websocket.send_json({
+                    "final_scores": scores
+                })
+                break
+
+    except Exception as e:
+        await websocket.close()
+        db.close()
+        print("WebSocket error:", e)
+    finally:
+        db.close()
+        await websocket.close()
+        print("Websocket 정상 종료")
+    
 
 @app.post("/get_subtitle", response_model=SubtitleResponse)
 async def get_subtitle(req: SubtitleRequest):
